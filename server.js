e0d229fdc7efe3a48ea086774dc45fca2df880e2
ricardofredes <ricardofredes@gmail.com>
2018-10-04 18:18:14 -0300

    First Commit

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express')
   ,expressLayouts = require('express-ejs-layouts')
-  ,app = express()
-  ,port = 8080
-  ,staticDirname = '/public';
+  ,routes = require('./controllers/routes.js');
+
+var app = express();
+var port = 8080;
+var staticDirname = '/public';
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-app.use(staticDirname, express.static(__dirname + '/static'));
+app.use(staticDirname, express.static(__dirname + '/public'));
+console.log(routes)
+
+/*
 app.use('/', function(req, res){
   var pathname = req.url;
   res.render(__dirname + '/views/pages' + pathname, {static: staticDirname, pathname});
 });
-
+*/
 app.listen(port);
