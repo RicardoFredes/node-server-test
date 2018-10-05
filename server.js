4b7e6965546e19971261c5acf2dad803274b7707
ricardofredes <ricardofredes@gmail.com>
2018-10-05 18:20:35 -0300

    First requests

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-var express = require('express')
-  ,expressLayouts = require('express-ejs-layouts')
-  ,routes = require('./controllers/routes.js');
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 
-var app = express();
-var port = 8080;
-var staticDirname = '/public';
+const port = process.env.PORT || 8080
+const staticDirname = '/public'
 
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
-app.use(staticDirname, express.static(__dirname + '/public'));
-console.log(routes)
+const app = express()
+const routes = require('./controllers')
 
-/*
-app.use('/', function(req, res){
-  var pathname = req.url;
-  res.render(__dirname + '/views/pages' + pathname, {static: staticDirname, pathname});
-});
-*/
-app.listen(port);
+app.set('view engine', 'ejs')
+
+app.use(expressLayouts)
+app.use(staticDirname, express.static(__dirname + '/public'))
+app.use('/', routes)
+
+app.listen(port)
