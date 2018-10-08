abc6b714cf469035117b3f41f4946856faed8c85
ricardofredes <ricardofredes@gmail.com>
2018-10-08 15:21:00 -0300

    Include styles

--- models/getBlogPosts.js
+++ models/getBlogPosts.js
@@ -28,6 +28,7 @@ const parseDataAndGetImages = data =>
         ({
           title: postItem.title.rendered,
           content: postItem.excerpt.rendered,
+          date: parseDate(postItem.date),
           slug: postItem.slug,
           image,
         })
@@ -54,5 +55,12 @@ const parseImageSize = image => ({
   height: image.height,
   width: image.width,
 })
+
+const mounth = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+
+const parseDate = date => {
+  const d = date.split('T')[0].split('-')
+  return `${d[2]} de ${mounth[Math.round(d[1]) - 1]} de ${d[0]}`
+}
 
 module.exports = rpPosts
